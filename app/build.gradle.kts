2173604f7956d5ebbd4171834a864ae57153178b
Karim <karim@botminds.ai>
2025-07-11 14:37:30 +0530

    Version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.quranmp3"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "2.5"
+        versionCode = 5
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
